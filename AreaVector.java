b4b99a324107591d6a7c63c06aeb8c0b629d8690
Gustavito18 <lmayorgao@miumg.edu.gt>
2022-05-15 23:19:55 -0600

    Vectores

---
+++ AreaVector.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoclase;
+
+/**
+ *
+ * @author DELL
+ */
+public class AreaVector {
+     
+    public void vectorResultado(){
+        int numeros []= new int [100];
+        for (int i=0; i<numeros.length; i++){
+            numeros [i]=i+1;
+            System.out.println(numeros[i]);
+        }
+    }
+}
